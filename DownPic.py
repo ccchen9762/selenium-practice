4d2b3c8a98a06cd44772b73c2c34e589fd2b5c55
teddy <age39303@gmail.com>
2020-09-08 22:14:43 +0800

    output adjust

--- DownPic.py
+++ DownPic.py
@@ -35,8 +35,8 @@ entry_file_location.place(x=200, y=70, width=180, height=24)
 label_result=tk.Label(mainpage, text="Result :")
 label_result.place(x=50, y=150, height=30)
 #scrolledtext result
-result_st=scrolledtext.ScrolledText(mainpage)
-result_st.place(x=50, y=180, width=540, height=360)
+result_st=scrolledtext.ScrolledText(mainpage, state='disabled')
+result_st.place(x=50, y=180, width=700, height=360)
 #===========================================basic layout===========================================
 
 #========================================button & function========================================
@@ -45,40 +45,59 @@ def search_file():
     #global file_name #actually it's unambiguous, so don't need this line
     file_location.set(filedialog.askdirectory())
 #filedialog button
-buttonGO=tk.Button(mainpage, text="...", relief="raised", command=search_file)
-buttonGO.place(x=400, y=70, width=30, height=24)
+button_file=tk.Button(mainpage, text="...", relief="raised", command=search_file)
+button_file.place(x=400, y=70, width=30, height=24)
+
+#request
+def request_html():
+    #requests
+    try:
+        global requests1
+        requests1 = requests.get(url.get())
+        result_st.configure(state='normal')
+        result_st.insert('end', "request status code = " + str(requests1.status_code) + "\n...\n")
+        if requests1.status_code == 200: #if success
+            result_st.insert('end', "request successed\n...\n")
+        else: #if failed
+            result_st.insert('end', "request failed\n...\n")
+        result_st.insert('end', "=" * 50 + "\n...\n")
+        result_st.insert('end', requests1.text)
+        result_st.configure(state='disabled')
+    except:
+        result_st.configure(state='normal')
+        result_st.insert('end', "request failed, is url empty?\n...\n")
+        result_st.configure(state='disabled')
+    
+#request button
+button_request=tk.Button(mainpage, text="request", relief="raised", command=request_html)
+button_request.place(x=480, y=30, width=100, height=24)
 
 #download
 def start_download():
-    #requests
-    requests1 = requests.get(url.get())
-    result_st.insert('end', "request status code = " + str(requests1.status_code) + "\n")
-    if requests1.status_code == 200: #if success
-        result_st.insert('end', "request successed\n\n")
-        soup=BeautifulSoup(requests1.text,"lxml")
-        image=soup.find_all("div")
-        links=[]
-        for d in image:
-            if d.find('img'):        #再從div找img裡面的src  
-                result=d.find('img')['src']
-                #print(result)
-                links.append(result)
-        x=1
-        for link in links:
-            local = os.path.join(file_location.get() + "/" + str(x) + ".jpg")
-            result_st.insert('end', "Downloading pic to : \"" + file_location.get() + "/" + str(x) + ".jpg\"\n")
-            urlretrieve(link,local) #link是下載的網址 local是儲存圖片的檔案位址
-            x+=1
-            if x>3:
-                break
-        result_st.insert('end', "\nsuccessfully download " + str(x-1) + " pictures\n\n")
-        result_st.insert('end', "=" * 50 + "\n\n")
-    else: #if failed
-        result_st.insert('end', "request failed\n\n")
-        result_st.insert('end', "=" * 50 + "\n\n")
+    result_st.configure(state='normal')
+    result_st.insert('end', "finding img\n...\n")
+    soup=BeautifulSoup(requests1.text,"lxml") #html.parser, html5lib, lxml is faster
+    image=soup.find_all("div")
+    links=[]
+    for d in image:
+        if d.find('img'):        #再從div找img裡面的src  
+            result=d.find('img')['src']
+            #print(result)
+            links.append(result)
+    x=1
+    for link in links:
+        local = os.path.join(file_location.get() + "/" + str(x) + ".jpg")
+        result_st.insert('end', "Downloading pic to : \"" + file_location.get() + "/" + str(x) + ".jpg\"\n")
+        urlretrieve(link,local) #link是下載的網址 local是儲存圖片的檔案位址
+        x+=1
+        if x>3:
+            break
+    result_st.insert('end', "\nsuccessfully download " + str(x-1) + " pictures\n...\n")
+    result_st.insert('end', "=" * 50 + "\n\n")
+    result_st.configure(state='disabled')
 #download button
 button_down=tk.Button(mainpage, text="Start Download", relief="raised", command=start_download)
-button_down.place(x=600, y=70, width=100, height=24)
+button_down.place(x=480, y=70, width=100, height=24)
 #========================================button & function========================================
 
 mainpage.mainloop()
